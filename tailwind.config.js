3f2ede6838c8503dbc2227ad50320c9085b07723
Prabal das <prabaldas421@gmail.com>
2026-04-05 23:40:15 +0530

    fix Tailwind content paths to correctly include jsx/tsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+content: [
+  "./index.html",
+  "./src/**/*.js",
+  "./src/**/*.jsx",
+  "./src/**/*.ts",
+  "./src/**/*.tsx",
+],
   theme: {
     extend: {
       fontFamily: {
